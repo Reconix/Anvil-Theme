317005577c9aa82ba075edb2ce456e850769cf1c
Reconix <Reconix@users.noreply.github.com>
2022-03-22 22:05:02 +0000

    Template Includes

---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+  <!-- left -->
+  <strong>Copyright &copy; 2022 <a href="#">Company</a>.</strong> All rights reserved. | Other Info
+  <!-- right -->
+  <div class="pull-right hidden-xs">
+    Anvil v2.0 beta
+  </div>
+</footer>
---
+++ inc/main-sidebar.php
@@ -0,0 +1,128 @@
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+
+  <!-- sidebar: style can be found in sidebar.less -->
+  <section class="sidebar">
+
+    <!-- Sidebar user panel (optional) -->
+    <div class="user-panel">
+      <div class="pull-left image">
+        <img src="assets/img/avatar_small.png" class="img-circle" alt="User Image">
+      </div>
+      <div class="pull-left info">
+        <p>Administrator</p>
+        <!-- Status -->
+        <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+      </div>
+    </div>
+
+    <!-- Sidebar Menu -->
+    <ul class="sidebar-menu" data-widget="tree">
+      <li class="header text-uppercase">Test Area</li>
+      <!-- Optionally, you can add icons to the links -->
+      <li><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
+      <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Active Link</span></a></li>
+      <li><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
+      <li class="treeview">
+        <a href="#"><i class="fa fa-link"></i> <span>Multilevel Test</span>
+          <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="#">Link lv 2</a></li>
+          <li><a href="#">Link lv 2</a></li>
+        </ul>
+      </li>
+      <!-- MODULES -->
+      <li class="header text-uppercase">Modules</li>
+
+      <li class="treeview">
+        <a href="#"><i class="fa fa-files-o"></i> <span>Pages</span>
+          <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="#">Link in level 2</a></li>
+          <li><a href="#">Link in level 2</a></li>
+        </ul>
+      </li>
+
+      <li class="treeview">
+        <a href="#"><i class="fa fa-newspaper-o"></i> <span>News</span>
+          <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="#">Link in level 2</a></li>
+          <li><a href="#">Link in level 2</a></li>
+        </ul>
+      </li>
+
+      <li class="treeview">
+        <a href="#"><i class="fa fa-list-alt"></i> <span>Forum</span>
+          <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="#">Link in level 2</a></li>
+          <li><a href="#">Link in level 2</a></li>
+        </ul>
+      </li>
+
+      <li class="treeview">
+        <a href="#"><i class="fa fa-shopping-cart"></i> <span>Shop</span>
+          <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="#">Link in level 2</a></li>
+          <li><a href="#">Link in level 2</a></li>
+        </ul>
+      </li>
+
+      <li class="treeview">
+        <a href="#"><i class="fa fa-calendar"></i> <span>Events</span>
+          <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="#">Link in level 2</a></li>
+          <li><a href="#">Link in level 2</a></li>
+        </ul>
+      </li>
+
+      <!-- System -->
+      <li class="header text-uppercase">System</li>
+
+      <li class="treeview">
+        <a href="#"><i class="fa fa-gears"></i> <span>Components</span>
+          <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="#">Link in level 2</a></li>
+          <li><a href="#">Link in level 2</a></li>
+        </ul>
+      </li>
+
+      <li><a href="#"><i class="fa fa-gears"></i> <span>Settings</span></a></li>
+
+      <!-- Testing -->
+      <li class="header text-uppercase">Another Area</li>
+
+      <li><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
+      <li><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
+      <li><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
+      <li><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
+    </ul>
+    <!-- /.sidebar-menu -->
+  </section>
+  <!-- /.sidebar -->
+</aside>
